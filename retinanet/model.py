e72c1bb27773f4908647eab66f84238d3ac5bcce
liyao <1599208165@qq.com>
2024-02-06 20:06:25 +0800

    update model.py

--- retinanet/model.py
+++ retinanet/model.py
@@ -55,7 +55,7 @@ class ResNet(nn.Module):
         # TODO: Please substitute the "?" to declare Focal Loss
         ##################################################################
 
-        self.focalLoss = "?"
+        self.focalLoss = losses.FocalLoss()
 
         ##################################################################
 
@@ -71,7 +71,11 @@ class ResNet(nn.Module):
         #####################################################################
         downsample = nn.Sequential()
         if stride != 1 or self.inplanes != planes * block.expansion:
-            pass
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
 
         ##################################################################
 
